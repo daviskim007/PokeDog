7af4019fbd80ffa22a2f7f6925a48b442ae0912a
daviskim007 <daviskim007@gmail.com>
2019-11-25 15:30:13 +0900

    initial commit

---
+++ app/src/main/java/com/example/pokedog/Controller/LogoActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pokedog.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.pokedog.R
+import kotlinx.android.synthetic.main.activity_logo.*
+
+class LogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+
+    }
+
+}
