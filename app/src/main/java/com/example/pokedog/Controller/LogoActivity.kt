e67b67649c8a8586d334c9aa5d2cf32a6db78373
daviskim007 <daviskim007@gmail.com>
2019-11-25 21:40:13 +0900

    2nd commit

--- app/src/main/java/com/example/pokedog/Controller/LogoActivity.kt
+++ app/src/main/java/com/example/pokedog/Controller/LogoActivity.kt
@@ -1,16 +1,20 @@
 package com.example.pokedog.Controller
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import com.bumptech.glide.Glide
 import com.example.pokedog.R
-import kotlinx.android.synthetic.main.activity_logo.*
 
 class LogoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logo)
+
+        Glide
+            .with(this)
+            .load(R.raw.logo)
+            .into(findViewById(R.id.loading_logo))
+
 
     }
 
